0aa18de642e5ae01c80f791d298b8509797a2f80
Adarsh R Shankar <adarshrshankar183@gmail.com>
2024-12-18 12:23:57 +0530

    add

--- app/src/main/java/com/fisat/newcalculator/MainActivity.java
+++ app/src/main/java/com/fisat/newcalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.fisat.newcalculator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,26 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2,b3,b4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.add);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob=new Intent(getApplicationContext(), add.class);
+                startActivity(ob);
+            }
+        });
+
+
 
     }
 }
